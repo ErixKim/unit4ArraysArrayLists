957d1331024448124a5d3ad2f78d048eb07fa415
ErixKim <edkim@stu.naperville203.org>
2015-11-20 13:30:08 -0600

    Practice 11/20/15

---
+++ Chapter 7 Practice/ArrayOperations.java
@@ -0,0 +1,27 @@
+public class ArrayOperations
+{
+    public static void main(String[]args)
+    {
+        double[] x = {8,4,5,21,7,9,18,2,100};
+        System.out.println(x.length);
+        System.out.println(x[0]);
+        System.out.println(x[8]);
+        System.out.println(x[x.length-1]);
+        for (int i = 0; i < 9; i++)
+        {
+            System.out.println(x[i]);
+        }
+        for (int i = 0; i < 9; i++)
+        {
+            System.out.println("x: " + x[i]);
+        }
+        for (int i = 8; i >= 0; i--)
+        {
+            System.out.println("x: " + x[i]);
+        }
+        for (double z : x)
+        {
+            System.out.println(z);
+        }
+    }
+}
