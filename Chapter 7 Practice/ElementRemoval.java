9f07f31bbeb126be4d8eb4f1ac1710b4d83d2d07
ErixKim <edkim@stu.naperville203.org>
2015-11-23 14:12:46 -0600

    Chapter 7 11/23

---
+++ Chapter 7 Practice/ElementRemoval.java
@@ -0,0 +1,28 @@
+public class ElementRemoval
+{
+    int searchValue = 100;
+    int[] values = { 80, 90, 100, 120, 110 };
+    int pos = 0;
+    boolean found = false;
+    while ( pos < values.length && !found )
+    {
+        if ( values[pos] == searchedValue )
+        {
+            found = true;
+        }
+        else
+        {
+            pos++;
+        }
+    }
+
+    if ( found )
+    {
+        System.out.println("Found at position: " + pos);
+    }
+    else
+    {
+        System.out.println("Not found");
+    }
+
+}
