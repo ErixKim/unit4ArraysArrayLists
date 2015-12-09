eaf7129ba4b0aaad41fd94abc50e4f4e27cea6b9
ErixKim <edkim@stu.naperville203.org>
2015-12-09 14:20:25 -0600

    Chapter 7 practice 12/9

---
+++ Chapter 7 Practice/Customer.java
@@ -0,0 +1,9 @@
+public class Customer
+{
+    private String customerName;
+    private double amount;
+    public Customer(String customerName, double amount)
+    {
+        Customer customer = new Customer(customerName, amount);
+    }
+}
--- Chapter 7 Practice/Store.java
+++ Chapter 7 Practice/Store.java
@@ -1,23 +1,22 @@
 import java.util.ArrayList;
 public class Store
 {
-    private ArrayList<String> list1 = new ArrayList<String>();
-    private ArrayList<Double> list2 = new ArrayList<Double>();
+    
+    private ArrayList<Customer> list1 = new ArrayList<Customer>();
+    
     public void addSale(String customerName , double amount)
     {
-        list1.add(customerName);
-        list2.add(amount);
+        list1.add(customerName, amount);
     }
     
     public String nameOfBestCustomer()
     {
-        double previousAmount = 0;
+        double largestValue = list2[0];
         for (int i = 0; i < list2.size(); i ++)
         {
-            double currentAmount = list2[i];
             if (currentAmount > previousAmount)
             {
-                double largestValue = currentAmount;
+                
             }
         }
     }
